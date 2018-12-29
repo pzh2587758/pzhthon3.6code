2fca62c3f839047151392f00292d59196359d401
pzh2587758 <hehe2587758@hotmail.com>
2018-12-29 21:55:21 +0800

    用笨办法解决的彩票格式的问题。

--- zjjc_3D中奖检测/PaiXu_NewNum.py
+++ zjjc_3D中奖检测/PaiXu_NewNum.py
@@ -6,11 +6,19 @@ class zjjc(object):
 
     def paixu_NewNum(self,old_num):
         """正向排序号码"""
+        num_num = []
+        for n in old_num[3:]:
+            num_num.append(int(n))
+        print("3:")
+        print(num_num)
+        list0_3 = old_num[:3]
+        old_num = list0_3 + num_num
+        print(old_num)
         zjh_7 = old_num[3:8]
         zjh_7.sort()
-        zjh_2 = old_num[8:10]
+        zjh_2 = old_num[8:]
         zjh_2.sort()
         zjh_7.extend(zjh_2)
-        zjh_new = old_num[0:2]
+        zjh_new = old_num[0:3]
         zjh_new.extend(zjh_7)
         return zjh_new
